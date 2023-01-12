a4c65792e676f52c28cb7ea7b029c86425d427d6
shubhamsuryawanshi <shubham.suryawanshi@stud.fra-uas.de>
2023-01-12 17:35:04 +0100

    Adding stages in the backend API

--- main.py
+++ main.py
@@ -28,9 +28,24 @@ def resolve_references(doc):
 
     return output_string
 
+def show_ents(doc):
+    global ner_op_string
+    
+    if doc.ents:
+        
+        for ent in doc.ents:
+            # print(ent.text+' - ' +str(ent.start_char) +' - '+ str(ent.end_char) +' - '+ent.label_+ ' - '+str(spacy.explain(ent.label_)))
+            
+            if ent.label_ == 'ORG':
+                word_length = ent.end_char - ent.start_char
+                ner_op_string = ner_op_string[:ent.start_char] + 'O'*(word_length) + ner_op_string[ent.end_char:]
+    
+    else:
+        print('No named entities found.')
+    
+    return ner_op_string
 
 @app.get("/")
 async def get_goals(input_text):
     doc = coref_model(input_text)
     return {"goals": resolve_references(doc)}
-
